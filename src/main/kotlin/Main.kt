f6a44cfa4402b1d6d850145ee1e702992afaff56
chirchirmercy <cmercychptoo.com>
2023-02-06 07:27:17 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var fullName = "mercy cheptoo"
+    println(fullName)
+
+    var age = 21
+    println(age)
+
+    var phoneNumber = "0791554783"
+    println(phoneNumber)
+
+    var weightInKg = 50
+    println(weightInKg)
+
+    var kenyanCitizen: Boolean = true
+    println(kenyanCitizen)
+}
